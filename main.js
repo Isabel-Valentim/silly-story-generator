c4ef850e174cebf6c87e7d406f677ac1b06c1d8e
Isabel <isabelvalentim18@gmail.com>
2022-07-21 07:20:36 -0300

    custom name

---
+++ main.js
@@ -0,0 +1,31 @@
+const inputName = document.querySelector('#name')
+const btn = document.querySelector('#btn')
+const storySpace = document.querySelector('#story')
+
+const insert = inputName.value
+
+const myarray =[
+
+`It was 94 fahrenheit outside, so ${insert} went for a walk. When they got to ${insert}, they stared in horror for a few moments, then ${insert}. Bob saw the whole thing, but was not surprised — ${insert} weighs 300 pounds, and it was a hot day.`,
+
+"Willy the Goblin <br> Big Daddy <br> Father Christmas",
+
+"the soup kitchen <br> Disneyland <br> the White House",
+
+"spontaneously combusted <br> melted into a puddle on the sidewalk <br> turned into a slug and crawled away"
+]
+
+//In addition you've got a function called randomValueFromArray() that takes an array, and returns one of the items stored inside the array at random.
+
+
+let ranNum = Math.floor(Math.random() * (myarray.length)+1);
+
+btn.addEventListener('click', genRanStor)
+
+function genRanStor(){
+    ranNum = Math.floor(Math.random() * (myarray.length)+1);
+    console.log(ranNum)
+    storySpace.innerHTML = myarray[ranNum -1]  
+}
+    
+
